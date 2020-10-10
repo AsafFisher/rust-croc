79ca4bfcf9871bd91624e09ff0a4b8b9bc530430
Asaf Fisher <asaffisher@icloud.com>
2020-10-10 17:43:53 +0300

    croc: implement discovery callbacks.

--- src/discovery/mod.rs
+++ src/discovery/mod.rs
@@ -3,6 +3,7 @@ use std::default::Default;
 use std::io;
 use std::net::{IpAddr, SocketAddr, UdpSocket};
 use std::sync::{Arc, RwLock};
+
 // SSTP multicast address.
 const DEFAULT_V4_MULTICAST_ADDRESS: [u8; 4] = [239, 255, 255, 250];
 
@@ -32,10 +33,6 @@ pub struct Peer {
     data: Vec<u8>,
 }
 
-// impl Copy for Peer {
-//     fn copy()
-// }
-
 impl PartialEq for Peer {
     fn eq(&self, other: &Self) -> bool {
         return self.ip == other.ip;
@@ -46,10 +43,13 @@ impl PartialEq for Peer {
     }
 }
 
+#[derive(Clone)]
 pub struct Discovery {
     multicast_addr: SocketAddr,
     binding_addr: SocketAddr,
+    message: Vec<u8>,
     peerlist: Arc<RwLock<Vec<Peer>>>,
+    stopper: Arc<RwLock<bool>>,
 }
 
 impl Default for Discovery {
@@ -57,65 +57,60 @@ impl Default for Discovery {
         Self {
             multicast_addr: SocketAddr::from((DEFAULT_V4_MULTICAST_ADDRESS, DEFAULT_PORT_NUMBER)),
             binding_addr: SocketAddr::from((INADDR_ANY, DEFAULT_PORT_NUMBER)),
+            message: b"discover".to_vec(),
             peerlist: Arc::new(RwLock::new(Vec::new())),
+            stopper: Arc::new(RwLock::new(false)),
         }
     }
 }
 
 impl Discovery {
-    pub fn discover(&self) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn discover< C: Fn(&mut Vec<Peer>) + std::marker::Send + 'static >(&self, callback: C) -> Result<(), Box<dyn std::error::Error>> {
         let Discovery {
             multicast_addr,
             binding_addr,
-            peerlist: _,
-        } = *self;
-        let peerlist = self.peerlist.clone();
-        let stopper = Arc::new(RwLock::new(false));
+            message,
+            peerlist,
+            stopper,
+        } = self.clone();
+
+        // Setup stoppers
+        let (broadcast_stopper, receiver_stopper) = (stopper.clone(), stopper);
 
         // Create the discovery socket.
-        let receive_socket = UdpSocket::bind(binding_addr).expect("Fuck");
-
-        let send_socket = receive_socket
+        let send_socket = UdpSocket::bind(binding_addr).expect("Fuck");
+        let receive_socket = send_socket
             .try_clone()
             .expect("Could not clone the socket!");
-        let b_stopper = stopper.clone();
 
         // Start the broadcasting, pass the stopper.
         concurrent! {
-            broadcast(send_socket, &multicast_addr,
-            &b_stopper).unwrap();
+            broadcast(send_socket, &multicast_addr, message,
+            &broadcast_stopper).unwrap();
         };
 
         // Listen for broadcast.
         concurrent! {
-            listen(
+            receiver(
                 receive_socket,
                 multicast_addr.ip(),
                 binding_addr.ip(),
                 &peerlist,
-                &stopper,
+                callback,
+                &receiver_stopper,
             ).unwrap();
         };
         Ok(())
-        // // Signal the broadcasting process to die.
-        // {
-        //     let mut w_stop = stopper.write().expect("Deadlock");
-        //     *w_stop = true;
-        // }
     }
-    pub fn get_peers(&self) -> Result<Vec<Peer>, ()>{
-        let peerlist_guard = self.peerlist.read().expect("Deadlock");
-        Ok((*peerlist_guard).clone())
-    }
+
 }
 
-
-
-fn listen(
+fn receiver<C: Fn(&mut Vec<Peer>) + std::marker::Send + 'static>(
     socket: UdpSocket,
     multicast_address: IpAddr,
     binding_addr: IpAddr,
     peerlist: &RwLock<Vec<Peer>>,
+    callback: C,
     stopper: &RwLock<bool>,
 ) -> Result<(), io::Error> {
     match (multicast_address, binding_addr) {
@@ -123,6 +118,7 @@ fn listen(
         (IpAddr::V6(mip), _) => socket.join_multicast_v6(&mip, 0), // How do i build it good...
         _ => panic!("Can't combine ipv4 and ipv6."),
     }?;
+
     let mut buff = [0u8; 65535];
     loop {
         let stop = stopper.read().expect("Deadlock");
@@ -130,6 +126,7 @@ fn listen(
             return Ok(());
         }
         let (amount, src) = socket.recv_from(&mut buff).unwrap();
+        println!("Got packet");
         let peer = Peer {
             ip: src.ip(),
             data: buff[0..amount].to_vec(),
@@ -137,6 +134,7 @@ fn listen(
         let mut peerlist_guard = peerlist.write().expect("Deadlock");
         if !peerlist_guard.contains(&peer) {
             peerlist_guard.push(peer);
+            callback(&mut (*peerlist_guard).clone())
         }
     }
 }
@@ -144,6 +142,7 @@ fn listen(
 fn broadcast(
     socket: UdpSocket,
     multicast_address: &SocketAddr,
+    message: Vec<u8>,
     stopper: &RwLock<bool>,
 ) -> Result<(), io::Error> {
     if cfg!(debug_assertions) {
@@ -154,7 +153,7 @@ fn broadcast(
         if *stop {
             return Ok(());
         }
-        socket.send_to(b"hello", multicast_address.to_string())?;
+        socket.send_to(&message, multicast_address.to_string())?;
         std::thread::sleep(std::time::Duration::new(1, 0));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 //mod cli;
 mod discovery;
 
-use discovery::Discovery;
+use discovery::{Discovery, Peer};
+use std::sync::{Arc, RwLock};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let peer_discovery: Discovery = Default::default();
-    peer_discovery.discover()?;
-    
-    std::thread::sleep(std::time::Duration::new(5, 0));
-    
-    let peers = peer_discovery.get_peers();
-    for peer in peers {
-        println!("{:?}", peer);
+    let lpeers: Arc<RwLock<Vec<Peer>>> = Arc::new(RwLock::new(Vec::new()));
+    let peers = lpeers.clone();
+    // peer_list was cloned and moved here
+    peer_discovery.discover(move |peer_list:&mut Vec<Peer>| {
+        let mut lock = peers.write().unwrap();
+        lock.clear();
+        lock.append(peer_list)
+    })?;
+
+    loop {
+        let lock = lpeers.read();
+        let peers = lock.unwrap();
+        if !peers.is_empty() {
+            println!("{:?}", peers);
+        }
     }
-    Ok(())
 }
