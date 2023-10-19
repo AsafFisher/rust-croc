16a471fd8968c9719a4cde3c3dfa67e79a32a962
Asaf Fisher <asaffisher.dev@gmail.com>
2023-10-19 06:47:07 +0300

    Remove `CrocMessage` until it will be relevant

--- src/proto/mod.rs
+++ src/proto/mod.rs
@@ -4,70 +4,13 @@ mod croc_raw;
 mod croc_enc;
 pub use croc_enc::EncryptedSession;
 pub use croc_raw::CrocProto;
-use crypto::{
-    aes::AesEncryptor,
-    pake::{Pake, PakePubKey, Role},
-};
-use rand::RngCore;
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use std::{
-    convert::{TryFrom, TryInto},
-    io::{Read, Write},
-};
 
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::{TcpStream, ToSocketAddrs},
-};
-
-
-pub trait CrocMessage {
-    fn process(&self) -> Result<Message>;
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct PakeMessage {
-    #[serde(rename = "b")]
-    bytes: String,
-    #[serde(rename = "b2")]
-    bytes2: String,
-}
-impl CrocMessage for PakeMessage {
-    fn process(&self) -> Result<Message> {
-        todo!()
-    }
-}
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(tag = "t")]
-pub enum Message {
-    #[serde(rename = "pake")]
-    Pake(PakeMessage),
-    #[serde(rename = "finished")]
-    Finished,
-}
-impl TryFrom<&[u8]> for Message {
-    type Error = serde_json::Error;
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-        serde_json::from_slice(value)
-    }
-}
-
-fn process_message(message: &[u8]) -> Result<Message> {
-    let msg: Message = message.try_into().unwrap();
-    // process_message
-    match msg {
-        Message::Pake(msg) => msg.process(),
-        Message::Finished => Ok(Message::Finished),
-    }
-}
 
 #[cfg(test)]
 mod test {
     use std::net::TcpStream;
 
     use crypto::pake::{Pake, Role};
-
-    use super::{process_message, CrocProto};
 
     #[test]
     fn test_ping_proto() {
